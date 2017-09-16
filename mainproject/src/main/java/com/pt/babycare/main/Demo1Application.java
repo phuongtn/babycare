e9a1b236274390630db08d7f07d24d526f55a25b
Phuong Tran <phuong.tran@evizi.com>
2017-09-16 09:36:39 +0700

    Starter project

---
+++ mainproject/src/main/java/com/pt/babycare/main/Demo1Application.java
@@ -0,0 +1,12 @@
+package com.pt.babycare.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+}
