d7844d0716b246d24ec75103b5ca6a2f4aeafc54
Phuong Tran <phuong.tran@evizi.com>
2017-10-02 08:18:24 +0700

    update schema.sql

--- mainproject/schema.sql
+++ mainproject/schema.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               10.2.8-MariaDB - mariadb.org binary distribution
+-- Server version:               5.7.18-log - MySQL Community Server (GPL)
 -- Server OS:                    Win64
 -- HeidiSQL Version:             9.4.0.5125
 -- --------------------------------------------------------
@@ -23,23 +23,16 @@ CREATE TABLE IF NOT EXISTS `children` (
   `childid` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `userid` bigint(20) unsigned NOT NULL,
   `name` varchar(250) COLLATE utf8mb4_vietnamese_ci NOT NULL,
-  `babytype` int(1) unsigned NOT NULL DEFAULT 0,
+  `babytype` int(1) unsigned NOT NULL DEFAULT '0',
   `dob` bigint(20) unsigned NOT NULL,
   `blood` int(1) unsigned DEFAULT NULL,
   `gender` int(1) unsigned DEFAULT NULL,
   `region` int(2) unsigned DEFAULT NULL,
-  `uerid` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`childid`),
   KEY `FKip3bbhfnbkhl98d1omlou2qya` (`userid`),
-  CONSTRAINT `FKip3bbhfnbkhl98d1omlou2qya` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+  CONSTRAINT `FK_children_user` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
 
--- Data exporting was unselected.
--- Dumping structure for table babycare.hibernate_sequence
-DROP TABLE IF EXISTS `hibernate_sequence`;
-CREATE TABLE IF NOT EXISTS `hibernate_sequence` (
-  `next_val` bigint(20) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
 
 -- Data exporting was unselected.
 -- Dumping structure for table babycare.session
@@ -55,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `session` (
   UNIQUE KEY `hardwareid` (`hardwareid`),
   KEY `FK_session_user` (`userid`),
   CONSTRAINT `FK_session_user` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=113 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
 
 -- Data exporting was unselected.
 -- Dumping structure for table babycare.user
@@ -67,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `dob` bigint(20) DEFAULT NULL,
   `provider` varchar(250) COLLATE utf8_vietnamese_ci DEFAULT NULL,
   PRIMARY KEY (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 -- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
