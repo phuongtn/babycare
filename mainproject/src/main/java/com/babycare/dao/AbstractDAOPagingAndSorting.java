32b7fa902fb15376b05dc027fb4c7fd6d4469935
Phuong Tran <phuong.tran@evizi.com>
2017-11-04 16:31:37 +0700

    update

---
+++ mainproject/src/main/java/com/babycare/dao/AbstractDAOPagingAndSorting.java
@@ -0,0 +1,10 @@
+/*package com.babycare.dao;
+
+import java.io.Serializable;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public abstract class AbstractDAOPagingAndSorting<T extends Serializable, K> extends AbstractJpaDao<T> implements PagingAndSortingRepository<T, K> {
+
+}
+*/
