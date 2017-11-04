3ab41c78fee6771743300a5b21009d1038f26a41
Phuong Tran <phuong.tran@evizi.com>
2017-11-04 16:34:45 +0700

    update

---
+++ mainproject/src/main/java/com/babycare/dao/IGenericPagingRepo.java
@@ -0,0 +1,11 @@
+/*package com.babycare.dao;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+@NoRepositoryBean
+public interface IGenericPagingRepo<T extends Serializable, K extends Object> extends JpaRepository<T, K> {
+
+}
+*/
---
+++ mainproject/src/main/java/com/babycare/dao/impl/PushMessagePagingAndSortingDAO.java
@@ -0,0 +1,21 @@
+/*package com.babycare.dao.impl;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import com.babycare.dao.AbstractDAOPagingAndSorting;
+import com.babycare.dao.IPushMessageDAO;
+import com.babycare.model.entity.PushMessageEntity;
+
+@Repository
+@Component
+@Qualifier("pushMessagePagingAndSortingDAO")
+public abstract class PushMessagePagingAndSortingDAO extends AbstractDAOPagingAndSorting<PushMessageEntity, String> implements IPushMessageDAO {
+
+	public PushMessagePagingAndSortingDAO() {
+		super();
+		setClazz(PushMessageEntity.class);
+	}
+}
+*/
